f561d1316fe52ca88d64c27c3470ce073fcc7d7a
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-08-01 14:26:40 +0600

    changeset: remove unnecessary lines from index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const { GooglePage } = require("./src/pages/google");
 const GoogleTest = require("./src/tests/googleTest");
-const { getDriver } = require("./src/utils/driver");
 
 const main = async () => {
-  //   const driver = getDriver("chrome");
-  //   const googlePage = new GooglePage(driver);
-  //   await googlePage.navigateToPage();
-  //   await googlePage.insertIntoTextField(googlePage.searchInputField, "test");
-  //   await googlePage.clickOnElement(googlePage.searchBtn);
-  //   await driver.close();
   const googleTest = new GoogleTest();
   await googleTest.TC_01();
   await googleTest.tearDown();
